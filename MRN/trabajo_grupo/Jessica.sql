c6da4f6b75ed5767e2fce89017441a6bf031157b
Fraile2 <alejandrofraile421@gmail.com>
2023-12-11 12:04:38 +0100

    aa

--- MRN/trabajo_grupo/Jessica.sql
+++ MRN/trabajo_grupo/Jessica.sql
@@ -1,9 +1,9 @@
 INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("IBE", "Iberia" )
 INSERT INTO aerolinea (id) VALUES ("VLG", "Vueling");
-INSERT INTO erolinea (id) VALUES ("EZY", "Eazy Jet");
-INSERT INTO erolinea (id) VALUES ("AEA", "Air Europa");
-INSERT INTO erolinea (id) VALUES ("SKU", "Sky Airline");
-INSERT INTO erolinea (id) VALUES ("AFR", "Air Frnace");
-INSERT INTO erolinea (id) VALUES ("AAL", "America Airline");
-INSERT INTO erolinea (id) VALUES ("DAL", "Delta Air Lines");
-INSERT INTO erolinea (id) VALUES ("UAE", "Emirates Airline");
+INSERT INTO aerolinea (id) VALUES ("EZY", "Eazy Jet");
+INSERT INTO aerolinea (id) VALUES ("AEA", "Air Europa");
+INSERT INTO aerolinea (id) VALUES ("SKU", "Sky Airline");
+INSERT INTO aerolinea (id) VALUES ("AFR", "Air Frnace");
+INSERT INTO aerolinea (id) VALUES ("AAL", "America Airline");
+INSERT INTO aerolinea (id) VALUES ("DAL", "Delta Air Lines");
+INSERT INTO aerolinea (id) VALUES ("UAE", "Emirates Airline");
