9c3cb296b7aab65322a64e82555b803ba1000de8
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2024-01-08 19:52:54 +0100

    Update Jessica.sql

--- MRN/trabajo_grupo/Jessica.sql
+++ MRN/trabajo_grupo/Jessica.sql
@@ -1,9 +1,9 @@
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("IBE", "Iberia" )
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("VLG", "Vueling");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("EZY", "Eazy Jet");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("AEA", "Air Europa");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("SKU", "Sky Airline");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("AFR", "Air Frnace");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("AAL", "America Airline");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("DAL", "Delta Air Lines");
-INSERT INTO aerolinea (id_aerolinea, nombre) VALUES ("UAE", "Emirates Airline");
+INSERT INTO aerolinea (nombre) VALUES ("Iberia" )
+INSERT INTO aerolinea (nombre) VALUES ("Vueling");
+INSERT INTO aerolinea (nombre) VALUES ("Eazy Jet");
+INSERT INTO aerolinea (nombre) VALUES ("Air Europa");
+INSERT INTO aerolinea (nombre) VALUES ("Sky Airline");
+INSERT INTO aerolinea (nombre) VALUES ("Air Frnace");
+INSERT INTO aerolinea (nombre) VALUES ("America Airline");
+INSERT INTO aerolinea (nombre) VALUES ("Delta Air Lines");
+INSERT INTO aerolinea (nombre) VALUES ("Emirates Airline");
